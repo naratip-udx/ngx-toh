35bfd10d72c705b7dc2b76da4b85b589575c4fd6
naratip-udx <nara_pua@hotmail.com>
2020-01-23 16:50:53 +0700

    :sparkles: Part 2: Build a master/detail page with a list of heroes.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../model/hero';
+import { HEROES } from '../model/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +8,14 @@ import { Hero } from '../model/hero';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = { id: 1, name: 'Windstorm' };
+  heroes = HEROES;
+  selectedHero: Hero = {};
 
   constructor() {}
 
   ngOnInit() {}
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 }

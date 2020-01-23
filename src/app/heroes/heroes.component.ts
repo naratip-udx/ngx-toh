384fb7c82867f9d062f6afbbecabb8f025469cdf
naratip-udx <nara_pua@hotmail.com>
2020-01-23 20:09:33 +0700

    :sparkles: Part 6: Use HTTP to retrieve and save hero data.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -19,4 +19,19 @@ export class HeroesComponent implements OnInit {
   getHeroes() {
     this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
   }
+
+  add(name: string) {
+    if (!name.trim()) {
+      return;
+    }
+
+    this.heroService.addHero({ name } as Hero).subscribe(hero => {
+      this.heroes.push(hero);
+    });
+  }
+
+  delete(hero: Hero) {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 }
